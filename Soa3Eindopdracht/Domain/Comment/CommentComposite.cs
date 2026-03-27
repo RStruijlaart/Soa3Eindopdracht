276d6a3617530f375e938c92d8dea1c69ba0d2a9
RStruijlaart <79460280+RStruijlaart@users.noreply.github.com>
2026-03-27 21:28:01 +0100

    quickfix

--- Soa3Eindopdracht/Domain/Comment/CommentComposite.cs
+++ Soa3Eindopdracht/Domain/Comment/CommentComposite.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Soa3Eindopdracht.Domain.Comment;
 public class CommentComposite : CompositeComment
 {
-    public CommentComposite(string body, ProjectMember author, BacklogItem.BacklogItem backlogItem) : base(body, author, backlogItem)
+    public CommentComposite(string body, ProjectMember author, BacklogItem.BacklogItem backlogItem, CompositeComment parent) : base(body, author, backlogItem, parent)
     {
     }
 }
